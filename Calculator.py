bcb8a4a826ff2c63c3b7bc9a53ad0e0736b70b32
Diego Damaso <diego.damaso@outlook.com>
2024-04-26 02:03:38 -0300

    My first solo project. Trying to do a simple calculator.

---
+++ Calculator.py
@@ -0,0 +1,33 @@
+continue_calculating = True
+while continue_calculating is True:
+    valid_operator = ["+", "-", "*", "/"]
+    operator = input(f"Enter the operation you want (+ - * /): ")
+    while operator not in valid_operator:
+        operator = input("Enter the operation you want (+ - * /): ")
+    try:
+        num1 = int(input("Enter the 1st number: "))
+    except:
+        print("Invalid input. Enter a number.")
+    try:
+        num2 = int(input("Enter the 2st number: "))
+    except:
+        print("Invalid input. Enter a number.")
+
+    if operator == "+":
+        result = num1 + num2
+        print(round(result, 3))
+    elif operator == "-":
+        result == num1 - num2
+        print(round(result, 3))
+    elif operator == "*":
+        result = num1 * num2
+        print(round(result, 3))
+    elif operator == "/":
+        result = num1 / num2
+        print(round(result, 3))
+    else:
+        print("")
+
+    continue_ = input("Continue calculating? (y/n): ")
+    if continue_ == "n":
+        continue_calculating = False
